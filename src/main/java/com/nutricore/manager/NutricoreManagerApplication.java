955dd4227491a6facf4facf6e24b0cdf8b578293
Alberto Vilar <albertovilar1@gmail.com>
2025-12-20 20:39:47 -0300

    chore: initial project setup with Spring Boot, H2 and Flyway
    Initial configuration of the NutriCore-Manager ecosystem, including Web, JPA, and Validation dependencies, and migration support with Flyway.

---
+++ src/main/java/com/nutricore/manager/NutricoreManagerApplication.java
@@ -0,0 +1,13 @@
+package com.nutricore.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricoreManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutricoreManagerApplication.class, args);
+	}
+
+}
