04dc2a2a2549bfb8ee851199923524a3fcc041e9
Alberto Vilar <albertovilar1@gmail.com>
2025-12-26 20:40:29 -0300

    refactor: refine Nutritionist service to match PatientService pattern

--- src/main/java/com/nutricore/manager/api/mappers/NutritionistMapper.java
+++ src/main/java/com/nutricore/manager/api/mappers/NutritionistMapper.java
@@ -4,6 +4,7 @@ import com.nutricore.manager.api.dto.NutritionistRequestDTO;
 import com.nutricore.manager.api.dto.NutritionistResponseDTO;
 import com.nutricore.manager.domain.entities.Nutritionist;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
@@ -13,5 +14,8 @@ public interface NutritionistMapper {
 
     NutritionistResponseDTO toResponse(Nutritionist entity);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     void updateEntityFromDto(NutritionistRequestDTO dto, @MappingTarget Nutritionist entity);
 }
