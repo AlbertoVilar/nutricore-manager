288d22f6bba839dd3babeb1bedaddcd436060414
Alberto Vilar <albertovilar1@gmail.com>
2025-12-27 16:25:20 -0300

    feat: implement NutritionGoal creation logic

--- src/main/java/com/nutricore/manager/domain/services/NutritionGoalService.java
+++ src/main/java/com/nutricore/manager/domain/services/NutritionGoalService.java
@@ -3,8 +3,11 @@ package com.nutricore.manager.domain.services;
 import com.nutricore.manager.api.dto.NutritionGoalRequestDTO;
 import com.nutricore.manager.api.dto.NutritionGoalResponseDTO;
 import com.nutricore.manager.api.mappers.NutritionGoalMapper;
+import com.nutricore.manager.domain.enums.goal.GoalStatus;
+import com.nutricore.manager.domain.exceptions.ResourceNotFoundException;
 import com.nutricore.manager.infrastructure.db.repositories.NutritionGoalRepository;
 import com.nutricore.manager.infrastructure.db.repositories.PatientRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,13 +21,22 @@ public class NutritionGoalService {
     private final PatientRepository patientRepository;
     private final NutritionGoalMapper nutritionGoalMapper;
 
+    @Transactional
     public NutritionGoalResponseDTO create(NutritionGoalRequestDTO request) {
         // TODO: Validate patient existence
+        var patient = patientRepository.findById(request.patientId())
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Paciente não encontrado com ID: " + request.patientId()));
         // TODO: Map DTO to entity
+        var entity = nutritionGoalMapper.toEntity(request);
+
         // TODO: Set default status/rules
-        // TODO: Save entity
+        entity.setPatient(patient);
+        entity.setStatus(GoalStatus.PLANNED);
+
+        entity = nutritionGoalRepository.save(entity);
         // TODO: Return response DTO
-        return null;
+        return nutritionGoalMapper.toResponse(entity);
     }
 
     public NutritionGoalResponseDTO update(Long id, NutritionGoalRequestDTO request) {
